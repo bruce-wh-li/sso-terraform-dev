42ef61e54992b00d6cf99ca1a68896857a5121d6
junminahn <36021827+junminahn@users.noreply.github.com>
2022-02-22 20:03:05 +0000

    Deploying to gh-pages from @ bcgov/sso-requests@f55eb39c361b13cf340c8f6081ef490e681c08a9 🚀

---
+++ _next/static/chunks/pages/my-dashboard-62919330ee2a888be1e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[899],{60466:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/my-dashboard",function(){return u(93794)}])}},function(n){n.O(0,[936,615,964,488,774,888,179],(function(){return _=60466,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
